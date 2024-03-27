e9bf450d09e8d8b4b48b9e7106f2d2ceefd48d9f
unknown <ononobip@gmail.com>
2024-03-27 16:12:16 +0100

    Update: slight reform

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React, { useContext } from 'react'
 const Navbar = () => {
     const {darkMode, toggleDarkMode} = useContext(Context)
   return (
-    <nav className='flex items-center justify-between w-full p-2 border border-t-0 border-l-0 border-r-0 border-gray-300 bg-white text-[#444] dark:bg-[#333] dark:text-white' id='nav'>
+    <nav className='fixed z-20 flex items-center justify-between w-full p-2 border border-t-0 border-l-0 border-r-0 border-gray-300 bg-white text-[#444] dark:bg-[#333] dark:text-white' id='nav'>
         <div className='w-full font-semibold '> Hunt </div>
         <div className='w-full flex items-center justify-end'>
         <button
